6221c3b3c18677f53362d23dfaf8198f443a673d
vpytlik <41046074+vpytlik@users.noreply.github.com>
2018-12-11 13:43:44 +0400

    Update test_ens3_interfaces.py

--- test_ens3_interfaces.py
+++ test_ens3_interfaces.py
@@ -6,14 +6,16 @@ nodes_without_ens3 = []
 
 
 class MyThread (threading.Thread):
-    def __init__(self, node, ens3_interfaces):
+    def __init__(self, node, ens3_interfaces, count):
         threading.Thread.__init__(self)
+        self.count = count
         self.node = node
         self.ens3_interfaces = ens3_interfaces
 
     def run(self):
-        print("Starting")
+        print("Start " + str(self.count))
         check_interfaces(self.node, self.ens3_interfaces)
+        print("End " + str(self.count))
 
 
 def test_check_ens3_interfaces(local_salt_client):
@@ -21,10 +23,12 @@ def test_check_ens3_interfaces(local_salt_client):
     ens3_interfaces = local_salt_client.cmd(
         "L@"+','.join(active_nodes), 'cmd.run', ['ifconfig ens3'], expr_form='compound')
     nodes = ens3_interfaces.keys()
+    count = 0
     for node in nodes:
-        thread = MyThread(node, ens3_interfaces)
+        thread = MyThread(node, ens3_interfaces, count)
         thread.setName(node)
         thread.start()
+        count += 1
     assert len(nodes_without_ens3) == 0, "Node is not contain ens3 interface: ".format(nodes_without_ens3)
 
 
